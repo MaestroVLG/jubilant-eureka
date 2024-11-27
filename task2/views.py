4d0a488d37028aae45367e0a30513c4021ee2c2a
MaestroVLG <las-vegas1945@yandex.ru>
2024-11-28 00:42:13 +0300

    2 задание

---
+++ task2/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views import View
+
+def function_view(request):
+    return render(request, 'second_task/function_template.html')
+
+class ClassView(View):
+    def get(self, request):
+        return render(request, 'second_task/class_template.html')
